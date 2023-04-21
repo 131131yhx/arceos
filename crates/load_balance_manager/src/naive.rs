8315ace4703486ad4cb3a79bd135c0cc140b49ee
叶昊星 <yehx20@mails.tsinghua.edu.cn>
2023-04-21 18:20:36 +0800

    暂存

--- crates/load_balance_manager/src/naive.rs
+++ crates/load_balance_manager/src/naive.rs
@@ -9,6 +9,7 @@ use scheduler::BaseScheduler;
 use crate::SimpleRunQueueOperations;
 use alloc::vec::Vec;
 use spinlock::SpinNoIrq; // TODO: 不确定！！！
+use log::info;
 //use std::marker::PhantomData;
 
 pub struct NaiveTask<Task, T> {
@@ -59,17 +60,20 @@ impl<Task, T, const SMP: usize> BaseManager for NaiveManager<Task, T, SMP> {
     type SchedItem = Arc<NaiveTask<Task, T>>;
     type InnerSchedItem = Arc<Task>;
     fn init(&mut self, cpu_id: usize, queue_ref: Arc<SpinNoIrq<dyn SimpleRunQueueOperations<SchedItem = Self::InnerSchedItem> + Send + 'static>>) {
+        info!("qwq1");
         self.scheduler_collection[cpu_id] = Some(queue_ref.clone());
         let mut scheduler = queue_ref.lock();
         queue_ref.lock().simple_init();
     }
 
     fn add_task(&mut self, cpu_id: usize, task: Self::SchedItem) {
-        self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_add_task(&task.inner)
+        info!("qwq2");
+        self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_add_task(&task.inner.clone())
     }
 
     fn remove_task(&mut self, cpu_id: usize, task: &Self::SchedItem) -> Option<Self::SchedItem> {
-        if let Some(inner) = self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_remove_task(&task.inner) {
+        info!("qwq3");
+        if let Some(inner) = self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_remove_task(&task.inner.clone()) {
             Some(Self::SchedItem::new(NaiveTask {
                 inner: inner.clone(),
                 _marker: None,
@@ -80,6 +84,7 @@ impl<Task, T, const SMP: usize> BaseManager for NaiveManager<Task, T, SMP> {
     }
 
     fn pick_next_task(&mut self, cpu_id: usize) -> Option<Self::SchedItem> {
+        info!("qwq4");
         if let Some(inner) = self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_pick_next_task() {
             Some(Self::SchedItem::new(NaiveTask {
                 inner: inner.clone(),
@@ -91,10 +96,12 @@ impl<Task, T, const SMP: usize> BaseManager for NaiveManager<Task, T, SMP> {
     }
 
     fn put_prev_task(&mut self, cpu_id: usize, prev: Self::SchedItem, _preempt: bool) {
-        self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_put_prev_task(&prev.inner, _preempt)
+        info!("qwq5 {}", Arc::strong_count(&prev.inner));
+        self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_put_prev_task(&prev.inner.clone(), _preempt)
     }
 
     fn task_tick(&mut self, cpu_id: usize, _current: &Self::SchedItem) -> bool {
+        info!("qwq6");
         self.scheduler_collection[cpu_id].as_ref().unwrap().lock().simple_task_tick(_current)
     }
 }
